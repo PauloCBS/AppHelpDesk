d0505b00e541def46f7c8c2c5c52583b2de55571
Your Name <paulo-cesar-batista@hotmail.com>
2022-06-04 20:06:15 +0100

    Validacao de usuario concluida, a maior dificuldade foi perceber que o metodo get e post recebem o name do campo de texto e isso gerou um erro pois o nome e-mail estava diferente do nome email solicitado pelo metodo POST

--- index.php
+++ index.php
@@ -40,6 +40,13 @@
                 <div class="form-group">
                   <input  name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
+                <?php if(isset($_GET['login']) && $_GET['login'] =='erro' ){ ?>
+                      <div class="text-danger">
+                        Usuario ou Senha incorreto
+                      </div>
+
+                <?php } ?>
+                    
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
--- valida_login.php
+++ valida_login.php
@@ -1,9 +1,42 @@
 <?php 
 
+//variavel apra verificar autenticacao;
+
+$valida_user = false;
+
+
+//recbe as informacoes do formulario
 print_r($_POST);
 echo'<br/>';
 echo $_POST['email'];
 echo'<br/>';
 echo $_POST['senha'];
 
+
+//dados salvos hardcode
+$usuarios = array(
+
+    array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+    array('email' => 'user@teste.com.br', 'senha' => 'abcd')
+);
+
+
+//comparacao de entrada vs dados salvos 
+//feito a juncao da validacao com a autenticacao 
+foreach($usuarios as $user){
+
+    if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+        $valida_user = true;
+    }
+}
+
+//validacao entre os dados de entrada vs aramzenados
+
+    if($valida_user){
+        echo 'usuario autenticado';
+    }else{
+    //header sera utilizado para retorno a pagina index.php.
+    // a informacao ao lado direito da interrogacao aparece no url da pagina. 
+    header('Location: index.php?login=erro');
+    }
 ?>
